bbe0f7d16ea2943a4149f574f249d76e788d5bc2
pavan <itzteja66@gmail.com>
2026-07-27 19:54:59 +0530

    app name will now correctly display as "HR Core"

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.vepcon.app',
-  appName: 'Vepcon',
+  appName: 'HR Core',
   webDir: 'dist'
 };
 
